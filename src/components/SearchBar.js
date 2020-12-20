9e6f9001602e4fc66548b4564ee10a7e701497cb
Katia <katia.punter@storyshareplatform.com>
2020-12-20 18:39:00 +0000

    Step onChange to state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchBar = () => {
+
+    const [ inputBoxValue, setInputBoxValue ] = useState("");
+
+    const handleSearchChange = ({ target: { value }}) => {
+        setInputBoxValue(value);
+    };
+
     return(
         <div className="search-bar ui segment">
             <form className="ui form">
                 <div className="field">
                     <label>Video Search</label>
-                    <input type="text" />
+                    <input
+                        type="text"
+                        value={ inputBoxValue }
+                        onChange = { handleSearchChange }
+                    />
                 </div>
             </form>
         </div>
